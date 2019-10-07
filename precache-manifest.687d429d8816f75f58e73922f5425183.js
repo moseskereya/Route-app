208478b01d211d120758fa889f448a3eb2d0ae2a
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-07 23:44:36 +0300

    Updates

---
+++ precache-manifest.687d429d8816f75f58e73922f5425183.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04903a71e3ceb87d462f0c1506f707a",
+    "url": "/Route-app/index.html"
+  },
+  {
+    "revision": "a5d50d38cdd0caf2c566",
+    "url": "/Route-app/static/css/main.86e5762a.chunk.css"
+  },
+  {
+    "revision": "fa9d7c5620959ed14f06",
+    "url": "/Route-app/static/js/2.9a812691.chunk.js"
+  },
+  {
+    "revision": "a5d50d38cdd0caf2c566",
+    "url": "/Route-app/static/js/main.5d14af47.chunk.js"
+  },
+  {
+    "revision": "e87889ae4a2e7a5e58cf",
+    "url": "/Route-app/static/js/runtime-main.df46c5ca.js"
+  }
+]);
